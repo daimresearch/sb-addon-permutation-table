6f55aa8044ec94a0d03af7ca2af84a11df98e0f7
dongyunLee <dongyun.lee@daimreasearch.com>
2023-05-19 17:02:54 +0900

    add docs page

---
+++ docs/assets/Header.stories-cb4fc4b5.js
@@ -0,0 +1,8 @@
+import{H as c}from"./Header-5acc50a0.js";import"./jsx-runtime-91a467a5.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./Button-adfefb74.js";const l={title:"Example/Header",component:c,tags:["autodocs"],parameters:{layout:"fullscreen"}},e={args:{user:{name:"Jane Doe"}}},r={};var a,o,s;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    user: {
+      name: 'Jane Doe'
+    }
+  }
+}`,...(s=(o=e.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};var t,n,m;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:"{}",...(m=(n=r.parameters)==null?void 0:n.docs)==null?void 0:m.source}}};const L=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,L as __namedExportsOrder,l as default};
+//# sourceMappingURL=Header.stories-cb4fc4b5.js.map
