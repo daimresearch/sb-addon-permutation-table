78c33df6e9e99f3e315b8d1029dd0bfd573cee8c
dongyunLee <dongyun.lee@daimreasearch.com>
2023-05-19 18:17:49 +0900

    add: eui,mui,antd showcase

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,6 +3,7 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import "antd/dist/reset.css";
 
 const preview: Preview = {
   parameters: {
---
+++ stories/antd/Button.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Button as AntdButton, ButtonProps as AndBtnProps } from "antd";
+
+// type ButtonProps = Pick<
+//   AndBtnProps,
+//   "type" | "block" | "disabled" | "ghost" | "loading" | "shape" | "size"
+// > & { label: string;};
+
+interface ButtonProps {
+  label?: string;
+  block?: boolean;
+  danger?: boolean;
+  disabled?: boolean;
+  ghost?: boolean;
+  loading?: boolean;
+  shape?: "circle" | "round" | "default";
+  size?: "large" | "middle" | "small";
+  type: "primary" | "ghost" | "dashed" | "link" | "text" | "default";
+}
+
+export const Button = ({
+  label,
+  block = false,
+  danger = false,
+  disabled = false,
+  ghost = false,
+  loading = false,
+  shape = "default",
+  size = "middle",
+  type = "default",
+  ...rest
+}: ButtonProps) => {
+  const props = { block, danger, disabled, ghost, loading, shape, size, type };
+  return (
+    <AntdButton {...rest} {...props}>
+      {label}
+    </AntdButton>
+  );
+};
