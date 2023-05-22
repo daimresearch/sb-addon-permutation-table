260427893480b0d392513c631560a3e16eb33042
dongyunLee <dongyun.lee@daimreasearch.com>
2023-05-22 12:07:14 +0900

    add package

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,6 +6,7 @@ const config: StorybookConfig = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "sb-addon-permutation-table",
   ],
   framework: {
     name: "@storybook/react-vite",
