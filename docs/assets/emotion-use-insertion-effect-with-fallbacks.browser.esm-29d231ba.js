7942866f893d4a2d526c2981f0ee2ffdfe1ef216
dongyunLee <dongyun.lee@daimreasearch.com>
2023-05-23 15:22:10 +0900

    🚀 Deploy : 2023/05/23

---
+++ docs/assets/emotion-use-insertion-effect-with-fallbacks.browser.esm-29d231ba.js
@@ -0,0 +1,2 @@
+import{r as s,a}from"./index-8db94870.js";var r=function(t){return t()},e=a["useInsertionEffect"]?a["useInsertionEffect"]:!1,f=e||r,o=e||s.useLayoutEffect;export{o as a,f as u};
+//# sourceMappingURL=emotion-use-insertion-effect-with-fallbacks.browser.esm-29d231ba.js.map
