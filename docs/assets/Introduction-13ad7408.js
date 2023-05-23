59c3e353a3dd608c43d2ed6c30f1393efa788eed
dongyunLee <dongyun.lee@daimreasearch.com>
2023-05-23 15:53:11 +0900

    🚀 Deploy : ver 0.8

---
+++ docs/assets/Introduction-13ad7408.js
@@ -0,0 +1,46 @@
+import{j as e,a as r,F as d}from"./jsx-runtime-91a467a5.js";import{M as l}from"./index-d8ff1933.js";import{u as i}from"./index-1d576ef5.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./iframe-b1acfd92.js";import"../sb-preview/runtime.js";import"./index-9921493f.js";import"./index-d475d2ea.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-ae8e40e8.js";import"./index-1a3285a9.js";import"./index-d37d4223.js";import"./setPrototypeOf-6f2f822b.js";import"./inheritsLoose-856773bf.js";import"./isNativeReflectConstruct-e378569d.js";import"./index-c41dfe6e.js";import"./_baseIsEqual-e0960ee2.js";import"./index-356e4a49.js";function o(t){const n=Object.assign({h1:"h1",p:"p",code:"code",a:"a",strong:"strong",ol:"ol",li:"li",ul:"ul",pre:"pre",h2:"h2"},i(),t.components);return r(d,{children:[e(l,{title:"Introduction"}),`
+`,e(n.h1,{id:"sb-addon-permutation-table",children:"SB Addon Permutation-Table"}),`
+`,e(n.p,{children:`Permutation Addon은 컴포넌트의 다양한 모습을 손쉽게 확인할 수 있는 Addon입니다.
+이 데모 페이지에서, Permutation Addon을 사용하는 방법을 확인할 수 있습니다.
+유명 프론트엔드 컴포넌트 라이브러리와도 완전히 호환되며, 직접 설계한 컴포넌트에도 적용할 수 있습니다.`}),`
+`,r(n.p,{children:[`사이드바에 있는 스토리들을 살펴보면서, Permutation Addon을 사용하는 방법을 익혀보세요.
+애드온이 어떻게 동작하는지는 `,e(n.code,{children:"stories"}),` 디렉토리에 있는 코드를 확인해보세요.
+`,e(n.a,{href:"https://componentdriven.org",target:"_blank",rel:"nofollow noopener noreferrer",children:e(n.strong,{children:"컴포넌트 기반"})})," 방식으로 UI를 구축하는 것을 권장합니다."]}),`
+`,e(n.p,{children:e(n.a,{href:"https://github.com/daimresearch/sb-addon-permutation-table",target:"_blank",rel:"nofollow noopener noreferrer",children:"Github"})}),`
+`,e(n.h1,{id:"get-started",children:"Get started"}),`
+`,e(n.p,{children:`이 라이브러리는 React로 작성되었으며, Storybook 7.X >= 에서 정상적으로 동작함을 확인하였습니다.
+이외의 환경에서는 정상적으로 동작하지 않을 수 있습니다.`}),`
+`,r(n.ol,{start:"0",children:[`
+`,e(n.li,{children:"Perequisites"}),`
+`]}),`
+`,r(n.ul,{children:[`
+`,e(n.li,{children:e(n.code,{children:"node>=16"})}),`
+`,r(n.li,{children:[e(n.code,{children:"yarn"})," or ",e(n.code,{children:"npm"})]}),`
+`,r(n.li,{children:["a working project with:",`
+`,r(n.ul,{children:[`
+`,e(n.li,{children:"react >= 16.8"}),`
+`]}),`
+`]}),`
+`]}),`
+`,r(n.ol,{children:[`
+`,e(n.li,{children:"Installation"}),`
+`]}),`
+`,r(n.p,{children:[e(n.code,{children:"npm install --save-dev sb-addon-permutation-table"}),`
+`,e(n.code,{children:"yarn add -D sb-addon-permutation-table"})]}),`
+`,r(n.ol,{start:"2",children:[`
+`,e(n.li,{children:"Resister the addon in main.transform"}),`
+`]}),`
+`,e(n.pre,{children:e(n.code,{className:"language-tyepscript",children:`
+const config = {
+  // ...
+  addons: ['sb-addon-permutation-table']
+}
+
+`})}),`
+`,r(n.ol,{start:"3",children:[`
+`,e(n.li,{children:"Add it to story!"}),`
+`]}),`
+`,r(n.p,{children:["see details in ",e(n.a,{href:"https://github.com/daimresearch/sb-addon-permutation-table#usage",target:"_blank",rel:"nofollow noopener noreferrer",children:"here"})]}),`
+`,e(n.h2,{id:"contribution-guidelines",children:"Contribution guidelines"}),`
+`,e(n.p,{children:"TODO"})]})}function P(t={}){const{wrapper:n}=Object.assign({},i(),t.components);return n?e(n,Object.assign({},t,{children:e(o,t)})):o(t)}export{P as default};
+//# sourceMappingURL=Introduction-13ad7408.js.map
---
+++ docs/assets/emotion-use-insertion-effect-with-fallbacks.browser.esm-ae8e40e8.js
@@ -0,0 +1,2 @@
+import{r as s,a}from"./index-8db94870.js";var r=function(t){return t()},e=a["useInsertionEffect"]?a["useInsertionEffect"]:!1,f=e||r,o=e||s.useLayoutEffect;export{f as a,o as u};
+//# sourceMappingURL=emotion-use-insertion-effect-with-fallbacks.browser.esm-ae8e40e8.js.map
